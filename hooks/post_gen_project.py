6d064ae0eefcda60e59415213f0c65c745852361
Colby Timm <timmcolby@gmail.com>
2024-08-11 22:11:23 -0700

    Create pre-gen hook to validate project settings

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,4 +4,4 @@ from pathlib import Path
 if __name__ == '__main__':
 
     if "{{cookiecutter.language}}" != "Python":
-        Path('{{cookiecutter.project_name_python}}').unlink()
+        Path('{{cookiecutter.project_name}}-python').unlink()
