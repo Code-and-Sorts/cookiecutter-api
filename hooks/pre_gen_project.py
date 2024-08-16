736d6b847a36042e26d23616446616db0a545fe5
Colby Timm <timmcolby@gmail.com>
2024-08-15 22:18:21 -0700

    Allow spaces in project name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 import re
 import sys
 
-PROJECT_NAME_REGEX = r"^[-a-zA-Z][-a-zA-Z0-9]+$"
+PROJECT_NAME_REGEX = r"^[-a-zA-Z ][-a-zA-Z0-9 ]+$"
 project_name = "{{cookiecutter.project_name}}"
 if not re.match(PROJECT_NAME_REGEX, project_name):
     print(
-        f"ERROR: The project name {project_name} is not a valid Python module name. Please do not use a _ and use - instead."
+        f"ERROR: The project name {project_name} is not a valid Python module name. Please do not use a _ and use - instead"
     )
     # Exit to cancel project
     sys.exit(1)
