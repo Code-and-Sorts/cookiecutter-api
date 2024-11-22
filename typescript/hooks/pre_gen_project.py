061fdd79b84d43e19fb05841906b0e5f38a7b8be
Colby Timm <timmcolby@gmail.com>
2024-11-22 14:31:54 -0800

    Add validation for project naming conventions in pre_gen_project.py

---
+++ typescript/hooks/pre_gen_project.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import re
+import sys
+
+PROJECT_NAME_REGEX = r"^[-a-zA-Z ][-a-zA-Z0-9 ]+$"
+project_name = "{{cookiecutter.project_name}}"
+if not re.match(PROJECT_NAME_REGEX, project_name):
+    print(
+        f"ERROR: The project name {project_name} is not a valid module name. Please do not use a _ and use - instead"
+    )
+    # Exit to cancel project
+    sys.exit(1)
+
+PROJECT_LOWER_CAMEL_REGEX = r"^[a-z]+(?:[A-Z][a-z]+)*$"
+project_lower_camel = "{{cookiecutter.project_lower_camel_name}}"
+if not re.match(PROJECT_LOWER_CAMEL_REGEX, project_lower_camel):
+    print(
+        f"ERROR: The project lower camel {project_lower_camel} is not valid camel case. Please create a name using camelCase."
+    )
+    # Exit to cancel project
+    sys.exit(1)
+
+PROJECT_CLASS_NAME_REGEX = r"^[A-Z][A-Za-z0-9]*$"
+project_class_name = "{{cookiecutter.project_class_name}}"
+if not re.match(PROJECT_CLASS_NAME_REGEX, project_class_name):
+    print(
+        f"ERROR: The project class name {project_class_name} is not a valid class name. Please create a class name using PascalCase."
+    )
+    # Exit to cancel project
+    sys.exit(1)
+
+PROJECT_ENDPOINT_REGEX = r"^[a-zA-Z0-9\-_]*$"
+project_endpoint = "{{cookiecutter.project_endpoint}}"
+if not re.match(PROJECT_ENDPOINT_REGEX, project_endpoint):
+    print(
+        f"ERROR: The project endpoint {project_endpoint} is not a valid REST endpoint name. Please do not use a space or _ and use - instead."
+    )
+    # Exit to cancel project
+    sys.exit(1)
