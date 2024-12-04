c1a0a0b30786353893766ba7abd96797a8a68059
Colby Timm <timmcolby@gmail.com>
2024-12-03 18:58:02 -0800

    Add mock HttpRequestData utility for unit tests

---
+++ dotnet/{{cookiecutter.project_class_name}}/{{cookiecutter.project_class_name}}.Api.Tests.Unit/tests/mocks.cs
@@ -0,0 +1,26 @@
+namespace KittyClaws.Api.Tests.Unit;
+
+using System;
+using System.IO;
+using System.Text;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Newtonsoft.Json;
+using NSubstitute;
+
+public static class Mocks
+{
+    public static HttpRequestData CreateHttpRequestData<T>(T {{cookiecutter.project_lower_camel_name}}Request, string restMethod = "GET")
+    {
+        var context = Substitute.For<FunctionContext>();
+        var body = JsonConvert.SerializeObject({{cookiecutter.project_lower_camel_name}}Request);
+        var bodyStream = new MemoryStream(Encoding.UTF8.GetBytes(body));
+
+        var request = Substitute.For<HttpRequestData>(context);
+        request.Body.Returns(bodyStream);
+        request.Method.Returns(restMethod);
+        request.Url.Returns(new Uri("http://localhost/api/endpoint"));
+
+        return request;
+    }
+}
